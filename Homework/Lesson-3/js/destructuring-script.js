b1e6830744d7cde00747def16fd16a9455870703
sdimons <sdimons@gmail.com>
2019-10-21 11:08:51 +0300

    Сделано ДЗ по Деструктурирующему присваиванию

---
+++ Homework/Lesson-3/js/destructuring-script.js
@@ -0,0 +1,42 @@
+//1. Деструктурирующее присваивание
+document.getElementById("btnDestructuring1").addEventListener('click', function () {
+    let user = {
+        name: "John",
+        years: 30
+    };
+ 
+    let {name, years: age, isAdmin = false} = user;
+
+    alert( name ); // John
+    alert( age ); // 30
+    alert( isAdmin ); // false
+});
+
+//2. Максимальная зарплата
+function topSalary(salaries) {
+    if ((Object.keys(salaries) || []).length == 0) {
+        return null;
+    }
+    let {maxKey = "", maxSalary = 0} = {};
+    for (let [key, value] of Object.entries(salaries)) {
+        if (value > maxSalary) {
+            maxSalary = value;
+            maxKey = key;
+        }
+    }
+    if (maxKey != "") {
+        return {[maxKey]: maxSalary};
+    }
+    return null;
+}
+document.getElementById("btnDestructuring2").addEventListener('click', function () {
+    let salaries = {
+        "John": 100,
+        "Pete": 300,
+        "Mary": 250
+      };
+    let result = topSalary(salaries);
+    
+    alert(`Исходные данные: ${JSON.stringify(salaries)}\n`+
+          `Результат: ${JSON.stringify(result)}`);
+});
