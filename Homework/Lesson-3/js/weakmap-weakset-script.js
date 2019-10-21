2007aae5744e3c338a40a65379fd73c081f15f33
sdimons <sdimons@gmail.com>
2019-10-21 13:31:07 +0300

    Добавлено решение ДЗ по WeakMap и WeakSet

--- Homework/Lesson-3/js/weakmap-weakset-script.js
+++ Homework/Lesson-3/js/weakmap-weakset-script.js
@@ -1,9 +1,38 @@
 //1. Хранение отметок "не прочитано"
 document.getElementById("btnWeakmapWeakset1").addEventListener('click', function () {
+    let messages = [
+        {text: "Hello", from: "John"},
+        {text: "How goes?", from: "John"},
+        {text: "See you soon", from: "Alice"}
+    ];
 
+    let readingMesages =  new WeakMap();
+    for(let key of messages) {
+        if (!readingMesages.has(key)) {
+            readingMesages.set(key, false);
+        }
+    }
+    readingMesages.set(messages[1], true);
+    alert('Результаты в консоли');
+    console.log(readingMesages);
 });
 
 //2. Хранение времени прочтения
 document.getElementById("btnWeakmapWeakset2").addEventListener('click', function () {
+    let messages = [
+        {text: "Hello", from: "John"},
+        {text: "How goes?", from: "John"},
+        {text: "See you soon", from: "Alice"}
+    ];
     
+    let whenReadingMesages =  new WeakMap();
+    for(let key of messages) {
+        if (!whenReadingMesages.has(key)) {
+            whenReadingMesages.set(key, null);
+        }
+    }
+
+    whenReadingMesages.set(messages[1], new Date());
+    alert('Результаты в консоли');
+    console.log(whenReadingMesages);
 });
