c6d1746b3f6a179ad143c8a3aa7ab95af2d5ea1a
sdimons <sdimons@gmail.com>
2019-10-30 17:44:31 +0300

    Подготовлены файлы под доп.задачу

--- Homework/Lesson-4/script.js
+++ Homework/Lesson-4/script.js
@@ -1,18 +1,11 @@
 'use strict';
 
 function highlight(table) {
-  console.log(table);
-  
-  let tbody = table.getElementsByTagName('tbody')[0];
-  let trs = tbody.getElementsByTagName('tr');
+   for(let row of table.rows) {
+      for(let cell of row.cells) {
 
- let a = 0;
- for (let item in trs) {
-     a++;
-     consolelog(a);
-     console.log(trs[item].cells[3]);
- }
-  console.log(trs);
+      }
+   }
 }
 
 highlight(document.querySelector('.js-teachers')); 
