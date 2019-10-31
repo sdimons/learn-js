3dca87db2ba87170263481db111b9e86594e5672
sdimons <sdimons@gmail.com>
2019-10-31 13:34:40 +0300

    Сделана доп. задача к уроку 4

--- Homework/Lesson-4/script.js
+++ Homework/Lesson-4/script.js
@@ -1,10 +1,31 @@
 'use strict';
 
 function highlight(table) {
-   for(let row of table.rows) {
-      for(let cell of row.cells) {
-
+   let tbody = table.tBodies[0];
+   for(let row of tbody.rows) {
+      let status = row.lastElementChild;
+      if (status && status.hasAttribute('data-available')) {
+         if (status.dataset.available === 'true') {
+            row.className = 'available';
+         } else {
+            row.className = 'unavailable';
+         }
       }
+      else {
+         row.hidden = 'hidden';
+      }
+      let gender = status.previousElementSibling;
+      if (gender && gender.innerText === 'm') {
+         row.classList.add('male');
+      }
+      if (gender && gender.innerText === 'f') {
+         row.classList.add('female');
+      }
+      let age = gender.previousElementSibling;
+      if (age && parseInt(age.innerText) < 18) {
+         row.style.cssText = 'text-decoration: line-through';
+      }
+
    }
 }
 
